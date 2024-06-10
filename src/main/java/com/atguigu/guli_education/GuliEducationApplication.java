17621f7127b78f55ac4ecae9b887648cd816ab20
Sunny-lucking <934778945@qq.com>
2024-06-10 23:12:41 +0800

    初始化

---
+++ src/main/java/com/atguigu/guli_education/GuliEducationApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliEducationApplication.class, args);
+    }
+
+}
